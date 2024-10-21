0dd8ba573edf4defe718f45cee47e9a81383d7de
k.kyou <k.kyou@arealess.co.jp>
2024-10-21 20:27:29 +0900

    不具合修正

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,19 +1,8 @@
-//importScripts('https://www.gstatic.com/firebasejs/10.5.2/firebase-app-compat.js')
-//importScripts('https://www.gstatic.com/firebasejs/10.5.2/firebase-messaging-compat.js')
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging/sw";
+importScripts('https://www.gstatic.com/firebasejs/10.5.2/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.5.2/firebase-messaging-compat.js')
 
 //firebase init
-//firebase.initializeApp({
-//    apiKey: "AIzaSyDBTMv1nPOPnWtz7v0eyIHBDnU5FYft_2M",
-//    authDomain: "nttditial--sff.firebaseapp.com",
-//    projectId: "nttditial--sff",
-//    storageBucket: "nttditial--sff.appspot.com",
-//    messagingSenderId: "633790392845",
-//    appId: "1:633790392845:web:4316fa37b819260f798344",
-//});
-
-const firebaseApp = initializeApp({
+firebase.initializeApp({
     apiKey: "AIzaSyDBTMv1nPOPnWtz7v0eyIHBDnU5FYft_2M",
     authDomain: "nttditial--sff.firebaseapp.com",
     projectId: "nttditial--sff",
@@ -23,8 +12,7 @@ const firebaseApp = initializeApp({
 });
 
 //get FCM
-//const messaging = firebase.messaging();
-const messaging = getMessaging(firebaseApp);
+const messaging = firebase.messaging();
 
 //run in background
 messaging.onBackgroundMessage((payload) => {
